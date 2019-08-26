3e24830e41cf3718f19a7394d050d4774d0dc77c
Kibrom1 <kibmit@gmail.com>
2019-08-26 11:44:27 -0400

    how to display data using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,33 @@
 import { Component } from '@angular/core';
 
+import { Hero } from './hero';
+
 @Component({
   selector: 'app-root',
   template: `
-  <h1> {{ title}}</h1>
+    <h1>{{ title}} </h1>
+    <h2> My favorite hero is: {{ myHero.name }} </h2>
+    <p> Heroes: </p>
+    <ul>
+      <li *ngFor="let hero of heroes">
+        {{ hero.name }}
+      </li>
+    </ul>
+    <p *ngIf="heroes.length > 3"> There are many heroes! </p>
+    <p> The sum of 1+1 is {{ 1 + 1 }} </p>
   `
 })
 export class AppComponent {
-  title = 'displaying-data';
+  title = 'Tour of Heroes';
+ 
+  heroes = [
+    new Hero(1, 'Windstorm'),
+    new Hero(2, 'Bombasta'),
+    new Hero(12, 'Magneta'),
+    new Hero(20, 'Tornado')
+  ];
+  myHero = this.heroes[0];
+  constructor(){
+   
+  }
 }
